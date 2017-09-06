b8c596b1f8bc12cf2e2c70d54dc3836b4f0f5452
Joe <xumuyu@conew.com>
2017-09-06 18:27:51 +0800

    third commit

---
+++ app/src/main/java/com/example/coolweather/gson/AQI.java
@@ -0,0 +1,16 @@
+package com.example.coolweather.gson;
+
+/**
+ * Created by pc on 2017/9/6.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity{
+
+        public String aqi;
+        public String pm25;
+    }
+}
